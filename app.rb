bb0773b7eeb48768da4904a50907117893f20cbb
anam <anam.mansoor@7vals.com>
2016-04-24 15:32:10 +0100

    added a get disruptions function and it's test

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
+require 'json'
 
 class DisruptionsMap < Sinatra::Base
   get '/' do
     haml :display_map
+  end
+
+  get '/get_disruptions' do
+    content_type :json
+    disruption_points = [
+      ['Bondi Beach', -33.890542, 151.274856],
+      ['Coogee Beach', -33.923036, 151.259052],
+    ]
+
+    effected_areas = [[-33.90542, 151.284856,-34.890542, 152.274856], [-34.12, 151.959052, -34.923036, 151.959052]]
+
+    {disruption_points: disruption_points, effected_areas: effected_areas}.to_json
   end
 end
 # AIzaSyD8eKPDmhw2kwEeNVG4RbqfBIXGtz-WZ54
--- tests/app_test.rb
+++ tests/app_test.rb
@@ -2,9 +2,10 @@ ENV['RACK_ENV'] = 'test'
  
 require 'minitest/autorun'
 require 'rack/test'
+require 'pry'
 require_relative '../app'
  
-class DisruptionsMapAppTest < Minitest::Test
+class DisruptionsMapAppTest < Minitest::Spec
   include Rack::Test::Methods 
  
   def app
@@ -15,5 +16,12 @@ class DisruptionsMapAppTest < Minitest::Test
     get '/'
     assert last_response.ok?
     assert last_response.body.include?('London Traffic Disruptions Map')
+  end
+
+  def test_get_disruptions
+    get '/get_disruptions'     
+    data = JSON.parse(last_response.body, symbolize_keys: true)
+    assert data["effected_areas"].size.must_equal 2
+    assert data["disruption_points"].size.must_equal 2
   end
 end
